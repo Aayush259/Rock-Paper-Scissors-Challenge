7ecd0590f9ee0d04cba97b59449d058f8ca63dc2
Square <aayushk4980@gmail.com>
2024-05-07 01:03:05 +0530

    Added the Rules component in the App component

--- src/App.js
+++ src/App.js
@@ -2,25 +2,39 @@ import './style.css';
 import React, {useState} from 'react';
 import Nav from './components/Nav';
 import State from './components/State';
+import Rules from './components/Rules';
 
 function App() {
   
   // Initializing state for score.
-  let [score, setScore] = useState(0)
+  let [score, setScore] = useState(0);
+
+  // Initializing useState for opacity (for main game) and rules.
+  let [opacity, setOpacity] = useState(1);
+  let [rules, setRules] = useState(<></>);
   
   // This function will updates the score.
   const updateScore = () => {
-      setScore(score + 1)
+      setScore(score + 1);
+  }
+
+  // This function decrease the opacity of whole app except the rules and display the rules on screen.
+  const RuleBtnHandler = () => {
+    setOpacity(0.5);
+    setRules(<Rules />);
   }
 
   return (
-    <div className="App flex">
-      <Nav Score={score} />
-      <State UpdateScore={updateScore} />
-      <button id='rules'>Rules</button>
-      <div id="warning">
-        <p>This app is supposed to be run on device whose width is more than 230px</p>
+    <div className='flex' style={{flexDirection: 'column', width: '100vw'}}>
+      <div className="App flex" style={{opacity: opacity}}>
+        <Nav Score={score} />
+        <State UpdateScore={updateScore} />
+        <button id='rules' onClick={RuleBtnHandler}>Rules</button>
+        <div id="warning">
+          <p>This app is supposed to be run on device whose width is more than 230px</p>
+        </div>
       </div>
+      {rules}
     </div>
   );
 }
