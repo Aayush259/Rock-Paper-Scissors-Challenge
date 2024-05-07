2f26d49a9151355cba000ba0afdfe186975ebc0e
Square <aayushk4980@gmail.com>
2024-05-07 21:21:33 +0530

    Added overlay behind rules to prevent other functions

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -3,16 +3,18 @@ import rulesImage from '../images/image-rules.svg';
 function Rules(props) {
     return (
         <>
-        <div id='rules-container' className='flex'>
-            <header id='rules-header' className='flex'>
-                <h2>Rules</h2>
-                <button id='close-btn' className='flex' onClick={props.CloseRules}>
-                    <div className='bar'></div>
-                    <div className='bar'></div>
-                </button>
-            </header>
-            <div id='rules-image'>
-                <img src={rulesImage} alt='Game Rules' />
+        <div className="overlay-screen flex">
+            <div id='rules-container' className='flex'>
+                <header id='rules-header' className='flex'>
+                    <h2>Rules</h2>
+                    <button id='close-btn' className='flex' onClick={props.CloseRules}>
+                        <div className='bar'></div>
+                        <div className='bar'></div>
+                    </button>
+                </header>
+                <div id='rules-image'>
+                    <img src={rulesImage} alt='Game Rules' />
+                </div>
             </div>
         </div>
         </>
